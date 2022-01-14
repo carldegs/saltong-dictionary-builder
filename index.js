065ec9c71029700cdc7a38ff4adfb3685c61f8bd
Carl de Guia <carl.2795@gmail.com>
2022-01-14 13:09:16 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import { promises as fs } from 'fs';
+
+const DICT_FILE = 'tagalog_dict.txt';
+const OUT_FILE = 'grouped_dict.txt';
+
+const parseDictionary = async () => {
+  const file = await fs.readFile(DICT_FILE);
+  const words = file.toString().replace(/\r/g, '').split('\n');
+
+  let groupedWords = Array.from(new Array(20)).map(() => []);
+  words.forEach((word) => {
+    groupedWords = Object.assign([], groupedWords, {
+      [word.length]: [...groupedWords[word.length], word],
+    });
+  });
+
+  // TODO: save to DB instead
+  await fs.writeFile(
+    OUT_FILE,
+    JSON.stringify({
+      4: groupedWords[4],
+      5: groupedWords[5],
+      7: groupedWords[7],
+    })
+  );
+};
+
+parseDictionary();
